3968f0445b165af41630ce1295e2aa5e3209ee75
yves <yves@lepidus.com.br>
2025-04-29 16:39:31 -0400

    Adds most recent submission retrieval.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#991
    
    Signed-off-by: yves <yves@lepidus.com.br>

--- RankingPlugin.inc.php
+++ RankingPlugin.inc.php
@@ -7,6 +7,7 @@ class RankingPlugin extends GenericPlugin {
 	public function register($category, $path, $mainContextId = NULL) {
 		$success = parent::register($category, $path);
 		if ($success && $this->getEnabled()) {
+			HookRegistry::register('TemplateManager::display', array($this, 'handleMetricsData'));
 		}
 		return $success;
 	}
@@ -17,5 +18,34 @@ class RankingPlugin extends GenericPlugin {
 
 	public function getDescription() {
 		return __('plugins.generic.rankingPlugin.description');
+	}
+
+	public function handleMetricsData($hookName, $args) {
+        $template = $args[1];
+
+		if ($template != 'frontend/pages/indexJournal.tpl') {
+			return false;
+		}
+
+		$templateMgr = $args[0];
+		$request = Application::get()->getRequest();
+
+		$limit = 10;
+
+		$mostRecentSubmissionsIterator = Services::get('submission')->getMany(
+            [
+                'contextId' => '1',
+                'status' => STATUS_PUBLISHED,
+                'orderDirection' => 'DESC',
+                'count' => $limit
+            ]
+        );
+
+        $submissionsInSections = [];
+        foreach ($mostRecentSubmissionsIterator as $submission) {
+			$submissionsInSections[]['articles'][] = $submission;
+        }
+
+		$templateMgr->assign('mostRecentSubmissions', $submissionsInSections);
 	}
 }
