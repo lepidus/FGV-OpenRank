ccf0b6c702d492a0101b17e45405f5e84a097a37
yves <yves@lepidus.com.br>
2025-05-14 19:08:31 -0400

    Implements Altmetrics API client.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#1001
    
    Signed-off-by: yves <yves@lepidus.com.br>

---
+++ classes/clients/Altmetrics.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\ServerException;
+
+class Altmetrics
+{
+    private const BASE_URL = ' https://api.altmetric.com/v1/';
+    private const ALTMETRICS_ENDPOINT = 'doi';
+    private $httpClient;
+
+    public function __construct($httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function fetchAltmetrics(string $doi): array
+    {
+        try {
+            $response = $this->httpClient->request(
+                'GET',
+                self::BASE_URL . self::ALTMETRICS_ENDPOINT . '/' . $doi
+            );
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (ServerException $error) {
+            error_log($error->getMessage());
+            throw new \Exception(__("plugins.generic.rankingPlugin.client.altmetrics.serverError"));
+        } catch (GuzzleException $error) {
+            throw new \Exception("Altmetrics Error: " . $error->getMessage(), 0, $error);
+        }
+    }
+}
